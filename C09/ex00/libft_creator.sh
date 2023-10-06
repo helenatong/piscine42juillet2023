85d9af4663d76a2b2c77f7550b98ea111035b24a
root <root@LAPTOP-VIGHRDQ9>
2023-10-06 21:23:13 +0200

    Suppression dossiers en double

--- C09/ex00/libft_creator.sh
+++
@@ -1,3 +0,0 @@
-cc -c *.c
-ar rc libft.a *.o
-ranlib libft.a
--- C12/ex01/ft_list.h
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-
-typedef struct  s_list
-{
-    struct s_list *next;
-    void          *data;
-}                 t_list;
